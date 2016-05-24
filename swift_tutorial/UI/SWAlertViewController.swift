183a681810c2dcf0d7e99ae2c65d7410797bc5da
Jonghyun Kim <filmhomage@hotmail.com>
2016-05-24 15:51:22 +0900

    Add to alert UI function

---
+++ swift_tutorial/UI/SWAlertViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SWAlertViewController.swift
+//  swift_tutorial
+//
+//  Created by JonghyunKim on 5/24/16.
+//  Copyright © 2016 kokaru.com. All rights reserved.
+//
+
+import Cocoa
+
+class SWAlertViewController: UIViewController {
+
+}
