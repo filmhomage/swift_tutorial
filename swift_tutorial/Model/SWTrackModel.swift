bfe491c1f03cba266b399e8e77cc2bbe73e262d4
Jonghyun Kim <filmhomage@hotmail.com>
2016-06-01 14:14:50 +0900

    Remove commenting out code.

--- swift_tutorial/Model/SWTrackModel.swift
+++ swift_tutorial/Model/SWTrackModel.swift
@@ -7,38 +7,11 @@
 //
 
 import Foundation
-import RealmSwift
 
 class SWTrackModel : Object {
-    
-//    static let realm = try! Realm()
     
     dynamic var trackName = ""
     dynamic var trackId = ""
     dynamic var artistName = ""
- 
-//    dynamic private var id = 0
-//    override static func primaryKey() -> String? {
-//        return "id"
-//    }
 
-//    static func loadAll() -> [SWTrackModel] {
-//        
-//        let tracks = realm.objects(SWTrackModel).sorted("id", ascending: false)
-//        var ret: [SWTrackModel] = []
-//        for track in tracks {
-//            ret.append(track)
-//        }
-//        return ret
-//    }
-//
-//    static func lastId() -> Int {
-//        if let track = realm.objects(SWTrackModel).last {
-//            return track.id + 1
-//        } else {
-//            return 1
-//        }
-//    }
-    
-    
 }
