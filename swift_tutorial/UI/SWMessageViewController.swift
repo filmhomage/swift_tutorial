281b0c37540b7dbb11a2f1bc7e94ddc7238d0a55
Jonghyun Kim <filmhomage@hotmail.com>
2016-06-21 13:44:50 +0900

    add messageViewController

---
+++ swift_tutorial/UI/SWMessageViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SWMessageViewController.swift
+//  swift_tutorial
+//
+//  Created by JonghyunKim on 6/21/16.
+//  Copyright © 2016 kokaru.com. All rights reserved.
+//
+
+import UIKit
+import JSQMessagesViewController
+
+class SWMessageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
